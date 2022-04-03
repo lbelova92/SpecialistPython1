9d179ea7f72c462dcd2409891b37403c109713d3
lbelova92 <102470962+lbelova92@users.noreply.github.com>
2022-04-03 12:37:29 +0300

    Update 01_task_while.py

--- Module2/practice/while/01_task_while.py
+++ Module2/practice/while/01_task_while.py
@@ -3,3 +3,9 @@
 # Формат выходных данных: Выведите все числа, требуемые по условию задачи.
 
 # TODO: your code here
+a=int(input("a: "))
+b=int(input("b: "))
+while a<=b:
+    if a%2==0:
+        print(a)
+    a+=1
