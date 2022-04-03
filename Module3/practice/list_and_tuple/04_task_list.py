bd224e158a0c11465654cd10468b5600da4e6d0d
lbelova92 <102470962+lbelova92@users.noreply.github.com>
2022-04-03 16:45:03 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,9 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+numbers = [-5,4,3,-10,10]
+summa=0
+for el in numbers:
+    if el>0:
+        summa=el+summa
+print(summa)
