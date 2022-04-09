5b820ab6776766f0d62403b050fd9ef476ffec5a
lbelova92 <102470962+lbelova92@users.noreply.github.com>
2022-04-09 21:40:13 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,19 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+from random import randint
+n=int(input('n:'))
+k=0
+summa=0
+numbers=[]
+for i in range(n):
+    numbers.append(randint(-100,100))
+print(numbers)
+#print(numbers[0])
+while k<n:
+    if numbers[k]>0 and numbers[k]%2==0:
+        summa=summa+numbers[k]
+    else:
+        summa=summa
+    k+=1
+print(summa)
